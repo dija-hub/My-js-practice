142c248245b17069f31b27a357cd2afb3a289969
dija <dija5408@gmail.com>
2025-08-21 20:10:49 +0500

    practice js

--- loops.js
+++ loops.js
@@ -1,3 +1,13 @@
-for(let count = 1; count <=10; count++){
-console.log("Apna College");
+// for(let count = 1; count <=5; count++){
+// console.log("Apna College");
+// }
+
+//new 1
+let sum =0;
+for (let i =1 ; i<=5 ; i++){
+ sum= sum+i;
 }
+ console.log("sum =", sum)
+console.log("loop has ended");
+
+
