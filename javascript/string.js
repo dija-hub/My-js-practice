3cd3015edc08de666a502a66414bed2868e63c0e
dija-hub <dija5408@gmail.com>
2025-08-27 12:00:00 +0500

    practice

--- javascript/string.js
+++ javascript/string.js
@@ -1,60 +1,60 @@
-let str="appna";
-let str2= 'khadija';
- str.length on console
-console.log(str.length);
-console.log(str[1]);
-console.log(str2[2]);
+// let str="appna";
+// let str2= 'khadija';
+//  str.length on console
+// console.log(str.length);
+// console.log(str[1]);
+// console.log(str2[2]);
 
-function countChars(str){
-  console.log("Length:", str.length);
-}
+// function countChars(str){
+//   console.log("Length:", str.length);
+// }
 
-countChars("JavaScript");
+// countChars("JavaScript");
 
-let sentence=`this is a template literal`;
-console.log(sentence);
-let obj={
-      item:pen,
-      price:30,
-};
-console.log("the cost of ",obj .items,"is", obj.price);
+// let sentence=`this is a template literal`;
+// console.log(sentence);
+// let obj={
+//       item:pen,
+//       price:30,
+// };
+// console.log("the cost of ",obj .items,"is", obj.price);
 
-let i=`the cost of ${obj.item}is ${obj.price} rupee`;
-console.log(i);
+// let i=`the cost of ${obj.item}is ${obj.price} rupee`;
+// console.log(i);
 
-let u=`this is a template literal ${1+2+3}`;
-console.log(u)
-//escape chara
-console.log("apna \ncollege");
-console.log("apna\tcollege");
+// let u=`this is a template literal ${1+2+3}`;
+// console.log(u)
+// //escape chara
+// console.log("apna \ncollege");
+// console.log("apna\tcollege");
 
-//string meathod 
-let str="appna";
-console.log(str.toUpperCase());
+// //string meathod 
+// let str="appna";
+// console.log(str.toUpperCase());
 
-let a="college"
-console.log(a.toLowerCase());
+// let a="college"
+// console.log(a.toLowerCase());
 
-let b="   appna    log      ";
-console.log(b.trim());
+// let b="   appna    log      ";
+// console.log(b.trim());
 
-let c="abcdefg"
-console.log(c.slice(2,6));
+// let c="abcdefg"
+// console.log(c.slice(2,6));
 
-console.log(str.concat(a));
+// console.log(str.concat(a));
 
-let d="kia hui?";
-console.log(d.replace("ui","ua"));
+// let d="kia hui?";
+// console.log(d.replace("ui","ua"));
 
-let e="i love js";
-console.log(e.charAt(3));
+// let e="i love js";
+// console.log(e.charAt(3));
 
-practice
+// practice
 
-username=prompt("enter your full name");
-console.log("username=",username);
-console.log("Username = " ,"@",username,username.length);
+// username=prompt("enter your full name");
+// console.log("username=",username);
+// console.log("Username = " ,"@",username,username.length);
 
- username=prompt("enter your full name");
- userid="@"+username+username.length;
- console.log(userid);
+//  username=prompt("enter your full name");
+//  userid="@"+username+username.length;
+//  console.log(userid);
