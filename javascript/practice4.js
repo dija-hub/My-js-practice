4b970b586df8e08bc9141d68924831dcb9194cfc
dija-hub <dija5408@gmail.com>
2025-08-31 12:00:00 +0500

    practice

--- javascript/practice4.js
+++ javascript/practice4.js
@@ -1,19 +1,19 @@
 
 
 
-function greet(){
-      let name="khadija"
-      function sayhi(){
-            console.log("helo",name)
-      }
-      return sayhi;
-}
-const hi =greet();
-hi();
+// function greet(){
+//       let name="khadija"
+//       function sayhi(){
+//             console.log("helo",name)
+//       }
+//       return sayhi;
+// }
+// const hi =greet();
+// hi();
 
-const[name,age]=["lilu",16]
-console.log(name);
-console.log(age);
+// const[name,age]=["lilu",16]
+// console.log(name);
+// console.log(age);
 
 // function counterMaker() {
 //   let count = 0;
