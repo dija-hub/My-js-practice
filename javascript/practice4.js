0231b1625cbdd04f13f2f34d6f919719f94d517a
dija-hub <dija5408@gmail.com>
2025-08-31 12:00:00 +0500

    practice

--- javascript/practice4.js
+++ javascript/practice4.js
@@ -11,9 +11,9 @@ function greet(){
 const hi =greet();
 hi();
 
-// const[name,age]=["lilu",16]
-// console.log(name);
-// console.log(age);
+const[name,age]=["lilu",16]
+console.log(name);
+console.log(age);
 
 // function counterMaker() {
 //   let count = 0;
