54f7d42341220cb3aa13eeab270dbfcd197567bf
dija <dija5408@gmail.com>
2025-09-04 20:24:13 +0500

    practice

--- loops.js
+++ loops.js
@@ -2,16 +2,21 @@
 // console.log("Apna College");
 // }
 
-//new 1
-let sum =0;
-for (let i =1 ; i<=5 ; i++){
- sum= sum+i;
-}
- console.log("sum =", sum)
-console.log("loop has ended");
-let i=1;
-while (i<=5){
-      console.log("i=",i);
-      i++;
-}
+// new 1
+// let sum =0;
+// for (let i =1 ; i<=5 ; i++){
+//  sum= sum+i;
+// }
+//  console.log("sum =", sum)
+// console.log("loop has ended");
+// let i=1;
+// while (i<=5){
+//       console.log("i=",i);
+//       i++;
+// }
 
+let i =1;
+do{
+      console.log("appna");
+      i++;
+} while(i<=10);
