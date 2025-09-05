c65b9e230324dc73d9c591069dcc7db94455681f
dija <dija5408@gmail.com>
2025-09-05 20:04:46 +0500

    practice

--- loops.js
+++ loops.js
@@ -39,19 +39,32 @@
 console.log( "Welcome to code cream shop!");
 icecream=prompt("How many ice creams would you like");
 console.log("How many ice creams would you like ?",icecream);
-if(icecream=1){
+if(icecream==1){
       flavor1=prompt("Enter flavor for ice cream 1:");
 console.log("Enter flavor for ice cream 1:",flavor1);
 }
-else if(icecream=2){
+else if(icecream<=2){
+        flavor1=prompt("Enter flavor for ice cream 1:");
+console.log("Enter flavor for ice cream 1:",flavor1);
 flavor2=prompt("Enter flavor for ice cream 2:");
 console.log("Enter flavor for ice cream 2:",flavor2);
 }
-else if(icecream=3){
+else if(icecream<=3){
+       flavor1=prompt("Enter flavor for ice cream 1:");
+console.log("Enter flavor for ice cream 1:",flavor1);
+flavor2=prompt("Enter flavor for ice cream 2:");
+console.log("Enter flavor for ice cream 2:",flavor2);
 flavor3=prompt("Enter flavor for ice cream 3:");
 console.log("Enter flavor for ice cream 3:",flavor3);
 }
-else if(icecream=4){
+else if(icecream<=4){
+       flavor1=prompt("Enter flavor for ice cream 1:");
+console.log("Enter flavor for ice cream 1:",flavor1);
+flavor2=prompt("Enter flavor for ice cream 2:");
+console.log("Enter flavor for ice cream 2:",flavor2);
+flavor3=prompt("Enter flavor for ice cream 3:");
+console.log("Enter flavor for ice cream 3:",flavor3);
 flavor4=prompt("Enter flavor for ice cream 4:");
 console.log("Enter flavor for ice cream 4:",flavor4);
 }
+console.log("your order:",flavour1,flavor2,flavor3,flavor4);
