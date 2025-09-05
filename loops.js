8528a65b6c98c3e6eadee9c26cdac34b9c91b041
dija <dija5408@gmail.com>
2025-09-05 19:56:47 +0500

    practice

--- loops.js
+++ loops.js
@@ -15,22 +15,43 @@
 //       i++;
 // }
 
-let i =1;
-do{
-      console.log("appna");
-      i++;
-} while(i<=10);
+// let i =1;
+// do{
+//       console.log("appna");
+//       i++;
+// } while(i<=10);
 
 
-for (let i =1;i <=5 ; i++){
-      console.log("count is"+i);
+// for (let i =1;i <=5 ; i++){
+//       console.log("count is"+i);
+// }
+// for (let countdown = 10; countdown >0; countdown--) {
+//     console.log(countdown +"...");
+// }
+
+// for (let i =1 ;i <= 9;i++ ){
+//  if (i %2 ===1){
+//       console.log(i);
+//  }
+// }
+
+//practice
+console.log( "Welcome to code cream shop!");
+icecream=prompt("How many ice creams would you like");
+console.log("How many ice creams would you like ?",icecream);
+if(icecream=1){
+      flavor1=prompt("Enter flavor for ice cream 1:");
+console.log("Enter flavor for ice cream 1:",flavor1);
 }
-for (let countdown = 10; countdown >0; countdown--) {
-    console.log(countdown +"...");
+else if(icecream=2){
+flavor2=prompt("Enter flavor for ice cream 2:");
+console.log("Enter flavor for ice cream 2:",flavor2);
 }
-
-for (let i =1 ;i <= 9;i++ ){
- if (i %2 ===1){
-      console.log(i);
- }
+else if(icecream=3){
+flavor3=prompt("Enter flavor for ice cream 3:");
+console.log("Enter flavor for ice cream 3:",flavor3);
+}
+else if(icecream=4){
+flavor4=prompt("Enter flavor for ice cream 4:");
+console.log("Enter flavor for ice cream 4:",flavor4);
 }
