5b5903eb3f53c9e4c0657e6391747967b4c46eb8
dija <dija5408@gmail.com>
2025-09-06 19:20:24 +0500

    practice

--- string.js
+++ string.js
@@ -4,3 +4,15 @@ let str2= 'khadija';
  //string indices n console
 console.log(str[1]);
 console.log(str2[2]);
+
+//template literrals 
+let sentence=`this is a template literal`;
+console.log(sentence);
+let obj={
+      item:pen,
+      price:30,
+};
+// console.log("the cost of ",obj .items,"is", obj.price);
+//best way of doing this
+let i=`the cost of ${obj.item}is ${obj.price} rupee`;
+console.log(i);
