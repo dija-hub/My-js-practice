ce8453a1e5df9811b4a14176a3e1e24418158f8e
dija <dija5408@gmail.com>
2025-09-06 19:29:31 +0500

    practice

--- string.js
+++ string.js
@@ -1,18 +1,21 @@
-let str="appna";
-let str2= 'khadija';
+// let str="appna";
+// let str2= 'khadija';
  //str.length on console
  //string indices n console
-console.log(str[1]);
-console.log(str2[2]);
+// console.log(str[1]);
+// console.log(str2[2]);
 
 //template literrals 
-let sentence=`this is a template literal`;
-console.log(sentence);
-let obj={
-      item:pen,
-      price:30,
-};
+// let sentence=`this is a template literal`;
+// console.log(sentence);
+// let obj={
+//       item:pen,
+//       price:30,
+// };
 // console.log("the cost of ",obj .items,"is", obj.price);
 //best way of doing this
-let i=`the cost of ${obj.item}is ${obj.price} rupee`;
-console.log(i);
+// let i=`the cost of ${obj.item}is ${obj.price} rupee`;
+// console.log(i);
+
+let u=`this is a template literal ${1+2+3}`;
+console.log(u)
