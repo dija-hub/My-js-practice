c7214eb70491110482cea8376ce37c18ee2174a5
dija <dija5408@gmail.com>
2025-09-07 20:58:17 +0500

    practice

--- arrays.js
+++ arrays.js
@@ -1,5 +1,8 @@
-let info=["ironman","thor","batman","hulk"];
-console.log(info);
-let marks =["doremon","chipa","antman"];
-console.log(marks);
-info[1]="java";
+let info=["ironman","thor","batman","hulk","chipa","antman"];
+// console.log(info);
+// let marks =["doremon","chipa","antman"];
+// console.log(marks);
+// info[1]="java";
+for(let i=0;i<info.length;i++){
+      console.log(info[i]);
+}
