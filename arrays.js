d22111782c80cb2474faf3a0629368c41222ad1c
dija <dija5408@gmail.com>
2025-09-07 21:03:48 +0500

    practice

--- arrays.js
+++ arrays.js
@@ -1,8 +1,17 @@
-let info=["ironman","thor","batman","hulk","chipa","antman"];
+// let info=["ironman","thor","batman","hulk","chipa","antman"];
 // console.log(info);
 // let marks =["doremon","chipa","antman"];
 // console.log(marks);
 // info[1]="java";
-for(let i=0;i<info.length;i++){
-      console.log(info[i]);
+// for(let i=0;i<info.length;i++){
+//       console.log(info[i]);
+// }
+//for of
+// for (let hero of info){
+//       console.log(hero);
+// }
+
+let cities =["lahore", "karachi", "pindi","gugao"];
+for(let city of cities){
+      console.log(city.toUpperCase());
 }
