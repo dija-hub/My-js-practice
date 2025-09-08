000e713c10ec6b0b33e842ad06e08f9b5144469a
dija <dija5408@gmail.com>
2025-09-08 19:12:36 +0500

    practice

--- arrays.js
+++ arrays.js
@@ -11,9 +11,19 @@
 //       console.log(hero);
 // }
 
-let cities =["lahore", "karachi", "pindi","gugao"];
-for(let city of cities){
-      console.log(city.toUpperCase());
-} 
-let marks=[85,97,44,37,76,60];
- 
+// let cities =["lahore", "karachi", "pindi","gugao"];
+// for(let city of cities){
+//       console.log(city.toUpperCase());
+// } 
+// let marks=[85,97,44,37,76,60];
+//  let sum=0;
+//  sum = marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
+//  avg = sum/sum.length;
+
+ let marks=[85,97,44,37,76,60];
+ let sum=0;
+ for(let val of marks){
+     sum+=val;
+ }
+ console.log(sum);
+ console.log(sum/marks.length);
