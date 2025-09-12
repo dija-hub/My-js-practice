5bcf5d186baf3f7e22afb95587741c93cfec3e62
dija <dija5408@gmail.com>
2025-09-12 20:51:54 +0500

    practice

--- functions.js
+++ functions.js
@@ -137,5 +137,16 @@
 
 let n= prompt ("enter a number :");
 let arr =[];
+for(let i=1; i<=n ; i++){
+    arr[i-1]=i;
+}
+console.log(arr);
+ let sum =arr.reduce((res,curr)=>{
+ return res+curr;
+ })
+ console.log(sum);
 
-             
+ let product= arr.reduce((res,curr)=>{
+    return res*curr;
+ })
+ console.log(product);
