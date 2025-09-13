744e53c6b29b73ca471500bcd09892cf477f4d14
dija <dija5408@gmail.com>
2025-09-13 20:59:28 +0500

    practice

--- dom.js
+++ dom.js
@@ -1,4 +1,14 @@
- let heading1 =document.getElementById("heading1");
- console.dir(heading1);
- let heading=document.getElementsByClassName("heading");
- console.log(heading)
+//  let heading1 =document.getElementById("heading1");
+//  console.dir(heading1);
+//  let heading=document.getElementsByClassName("heading");
+//  console.log(heading);
+
+let element = document.querySelector("p");
+console.dir(element);
+
+let element_1= document.querySelectorAll("h2");
+console.dir(element_1);
+
+let element_2 =document.querySelectorAll(".heading3")
+console.dir(element_2);
+              
