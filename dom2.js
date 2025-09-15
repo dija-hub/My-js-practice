aa90912e7e22e2366434c5db7cc46b4bf6455a49
dija <dija5408@gmail.com>
2025-09-15 21:00:32 +0500

    practice

--- dom2.js
+++ dom2.js
@@ -9,7 +9,16 @@
 // console.log(para.setAttribute("para","new class"));
 
 
-let div = document.querySelector("div");
-console.log(div);
-console.log(div.style);
+// let div = document.querySelector("div");
+// // console.log(div);
+// // console.log(div.style);
 
+// div.style.backgroundColor="green";
+// div.style.fontSize="40px";
+
+let newbtn= document.createElement("button");
+newbtn.innerText="hello";
+console.log(newbtn);
+
+let div = document.createElement("div");
+div.append(newbtn);
