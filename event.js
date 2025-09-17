20c1126a8c828405e2e0833ef48ad71bcef0220f
dija <dija5408@gmail.com>
2025-09-17 21:15:27 +0500

    practice

--- event.js
+++ event.js
@@ -1,30 +1,30 @@
-let btn1=document.querySelector("#btn1");
+// let btn1=document.querySelector("#btn1");
 
-// btn1.onclick=()=>{
-//       console.log("button was clicked");
-//       let a=25
-//       a++;
-//       console.log(a);
+// // btn1.onclick=()=>{
+// //       console.log("button was clicked");
+// //       let a=25
+// //       a++;
+// //       console.log(a);
+// // }
+
+// // let box=document.querySelector("#box");
+
+// // box.onclick=()=>{
+// //       console.log("box was clicked");  
+// // }
+
+// // btn1.onclick=(evt)=>{
+// //       console.log(evt);
+// //       console.log(evt.types);  
+// //       console.log(evt.clientX, evt.clientY);
+// // }
+
+
+// const handler1=()=>{
+//       console.log("handler1");
 // }
+// btn1.addEventListener('click',handler1)
+// //to remove
 
-// let box=document.querySelector("#box");
-
-// box.onclick=()=>{
-//       console.log("box was clicked");  
-// }
-
-// btn1.onclick=(evt)=>{
-//       console.log(evt);
-//       console.log(evt.types);  
-//       console.log(evt.clientX, evt.clientY);
-// }
-
-
-const handler1=()=>{
-      console.log("handler1");
-}
-btn1.addEventListerner('clicked',handler1)
-//to remove
-
-btn1.removeEventListener("click","handler1"
-);
+// btn1.removeEventListener("click",handler1
+// );
