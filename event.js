42798d4eaf37096467962dc5e03ac2003e658608
dija <dija5408@gmail.com>
2025-09-17 21:08:07 +0500

    practice

--- event.js
+++ event.js
@@ -7,16 +7,24 @@ let btn1=document.querySelector("#btn1");
 //       console.log(a);
 // }
 
-let box=document.querySelector("#box");
+// let box=document.querySelector("#box");
 
-box.onclick=()=>{
-      console.log("box was clicked");  
+// box.onclick=()=>{
+//       console.log("box was clicked");  
+// }
+
+// btn1.onclick=(evt)=>{
+//       console.log(evt);
+//       console.log(evt.types);  
+//       console.log(evt.clientX, evt.clientY);
+// }
+
+
+const handler1=()=>{
+      console.log("handler1");
 }
+btn1.addEventListerner('clicked',handler1)
+//to remove
 
-btn1.onclick=(evt)=>{
-      console.log(evt);
-      console.log(evt.types);  
-      console.log(evt.clientX, evt.clientY);
-}
-
-
+btn1.removeEventListener("click","handler1"
+)
