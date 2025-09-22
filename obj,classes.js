60e1a906e9c7fb96fe851c92dd245796b4f3894b
dija <dija5408@gmail.com>
2025-09-22 20:54:17 +0500

    pracice

--- obj,classes.js
+++ obj,classes.js
@@ -30,10 +30,10 @@
 //             console.log("tax rate is 10%");
 //       },
 // }
-const karanArjun={
-      salary:50000,
-        calcTax(){
-      console.log("tax rate is 20%");
-        }
-};
- karanArjun.__proto__=employee;
+// const karanArjun={
+//       salary:50000,
+//         calcTax(){
+//       console.log("tax rate is 20%");
+//         }
+// };
+//  karanArjun.__proto__=employee;
