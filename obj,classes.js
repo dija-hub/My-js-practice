0d2f45a4d8179d6b113ce2c8adf927d116f6a9d0
dija <dija5408@gmail.com>
2025-09-22 20:38:22 +0500

    pracice

--- obj,classes.js
+++ obj,classes.js
@@ -7,3 +7,19 @@ const student={
 };
 
 
+function Person(name) {
+  this.name = name;
+}
+
+// One copy of method is stored in prototype
+Person.prototype.sayHello = function() {
+  console.log("Hello " + this.name);
+};
+
+let p1 = new Person("Khadija");
+let p2 = new Person("Ayesha");
+
+console.log(p1.sayHello === p2.sayHello); // true (shared copy ✅)
+
+p1.sayHello(); // Hello Khadija
+p2.sayHello(); // Hello Ayesha
