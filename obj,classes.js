9ce2d6f1376819719defc2c99c91e864abfdc347
dija <dija5408@gmail.com>
2025-09-22 20:49:07 +0500

    pracice

--- obj,classes.js
+++ obj,classes.js
@@ -1,25 +1,39 @@
-const student={
-      name:"kahdija wajid",
-      marks:90.6,
-      printmarks:function(){
-            console.log("marks =",this.marks);
-      },
-};
+// const student={
+//       name:"kahdija wajid",
+//       marks:90.6,
+//       printmarks:function(){
+//             console.log("marks =",this.marks);
+//       },
+// };
 
 
-function Person(name) {
-  this.name = name;
-}
+// function Person(name) {
+//   this.name = name;
+// }
 
-// One copy of method is stored in prototype
-Person.prototype.sayHello = function() {
-  console.log("Hello " + this.name);
-};
+// // One copy of method is stored in prototype
+// Person.prototype.sayHello = function() {
+//   console.log("Hello " + this.name);
+// };
 
-let p1 = new Person("Khadija");
-let p2 = new Person("Ayesha");
+// let p1 = new Person("Khadija");
+// let p2 = new Person("Ayesha");
 
-console.log(p1.sayHello === p2.sayHello); // true (shared copy ✅)
+// console.log(p1.sayHello === p2.sayHello); // true (shared copy ✅)
 
-p1.sayHello(); // Hello Khadija
-p2.sayHello(); // Hello Ayesha
+// p1.sayHello(); // Hello Khadija
+// p2.sayHello(); // Hello Ayesha
+
+
+// const employee={
+//       calcTax(){
+//             console.log("tax rate is 10%");
+//       },
+// }
+// const karanArjun={
+//       salary:50000,
+//         calcTax(){
+//       console.log("tax rate is 20%");
+//         }
+// };
+//  karanArjun.__proto__=employee;
