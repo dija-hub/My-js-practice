98c7ab612f1ec83fffb3e240ed7c93bdd03d4595
dija <dija5408@gmail.com>
2025-09-25 19:54:43 +0500

    pracice

--- lecture12.js
+++ lecture12.js
@@ -1,12 +1,22 @@
-console.log("one");
-console.log("two");
-console.log("three");
+// console.log("one");
+// console.log("two");
+// console.log("three");
 
-function hello(){
-      console.log("hello");
-}
+// function hello(){
+//       console.log("hello");
+// }
 
-setTimeout(hello,2000);
+// setTimeout(hello,2000);
 
-console.log("four");
-console.log("five");
+// console.log("four");
+// console.log("five");
+
+// function sum (a,b){
+//       console.log(a+b);
+// }
+
+// function calculator (a,b,sumCallBack){
+//       sumCallback(a,b);
+// }
+// calculator(1,2, sum);
+
