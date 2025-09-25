d7b3d301439d2016593718960a748d4e17ec2586
dija <dija5408@gmail.com>
2025-09-25 19:26:23 +0500

    pracice

---
+++ lecture12.js
@@ -0,0 +1,12 @@
+console.log("one");
+console.log("two");
+console.log("three");
+
+function hello(){
+      console.log("hello");
+}
+
+setTimeout(hello,2000);
+
+console.log("four");
+console.log("five");
