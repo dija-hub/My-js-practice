dd7bc84415ca98c2a0a2e4e5c9cdabb561574eb0
dija <dija5408@gmail.com>
2025-10-01 19:59:00 +0500

    pracice

--- lecture13.js
+++ lecture13.js
@@ -1,7 +1,18 @@
-const URL="https://catfact.ninja/fact";
+// const URL="https://catfact.ninja/fact";
 
-const getFacts=async () => {
-      console.log("getting data......")
- let response =await fetch(URL);
-console.log(response.status);
-}
+// const getFacts=async () => {
+//       console.log("getting data......")
+//  let response =await fetch(URL);
+// console.log(response.status);
+// }
+
+const myPromise = new Promise((resolve, reject) => {
+  // some async or time-consuming task
+  const success = true; // or false
+
+  if (success) {
+    resolve("✅ It worked!");
+  } else {
+    reject("❌ Something went wrong.");
+  }
+});
