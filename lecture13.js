b820e9cf82ebb18b8cfb7d61fdae3b8db2abb832
dija <dija5408@gmail.com>
2025-10-01 19:39:23 +0500

    pracice

--- lecture13.js
+++ lecture13.js
@@ -1,3 +1,7 @@
 const URL="https://catfact.ninja/fact";
-let promise=fetch(URL);
-console.log(promise);
+
+const getFacts=async () => {
+      console.log("getting data......")
+ let response =await fetch(URL);
+console.log(response.status);
+}
