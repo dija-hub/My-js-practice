70bfeea3cb846bf0bda990bf702ffc5089f7b736
dija <dija5408@gmail.com>
2025-10-14 20:02:06 +0500

    practice

--- practice2.js
+++ practice2.js
@@ -16,6 +16,10 @@
 // }
 // else{console.log(num,"is smaller then 10")}
 
-for (let i=1;i<=5;i++){
-      console.log(i);
+// for (let i=1;i<=5;i++){
+//       console.log(i);
+// }
+
+for (let i = 1; i <= 10; i++) {
+  console.log("5 x " + i + " = " + 5 * i);
 }
