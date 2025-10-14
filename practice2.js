2f1e71a98c132efaecb0849ceef825273597919f
dija <dija5408@gmail.com>
2025-10-14 19:46:03 +0500

    practice

--- practice2.js
+++ practice2.js
@@ -9,3 +9,8 @@
 
 // let name= prompt("enter your name :") ;
 // alert ("Hello " + name);
+
+// let num=76;
+// if (num > 10){
+//       console.log(num ,"is greater then 10")
+// }else(console.log(num,"is smaller then 10"))
