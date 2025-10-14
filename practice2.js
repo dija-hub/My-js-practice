551d01b7ef949ca2afe4f336a730ccc7fc4faa4a
dija <dija5408@gmail.com>
2025-10-14 19:41:56 +0500

    practice

--- practice2.js
+++ practice2.js
@@ -1,6 +1,11 @@
 // alert("Hello KHADIJA");
+
 // console.log("welcome to js!")
-let a=3;
-let b=4;
-let result = a+b;
-console.log(result);
+
+// let a=3;
+// let b=4;
+// let result = a+b;
+// console.log(result);
+
+// let name= prompt("enter your name :") ;
+// alert ("Hello " + name);
