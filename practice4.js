3893d6ed7c6130378ea652660885f1bb67586102
dija <dija5408@gmail.com>
2025-11-05 19:59:49 +0500

    closers

--- practice4.js
+++ practice4.js
@@ -7,3 +7,13 @@ let x=50;
 function sum(){
       console.log(y+x);
 }
+
+function greet(){
+      let name="khadija"
+      function sayhi(){
+            console.log("helo",name)
+      }
+      return sayhi;
+}
+const hi =greet();
+hi();
