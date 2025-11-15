7349a72ff5b003f2d4f834e1dc36a8ffa199084e
dija-hub <dija5408@gmail.com>
2025-11-15 12:00:00 +0500

    practice

--- javascript/practice6.js
+++ javascript/practice6.js
@@ -8,6 +8,8 @@ let a = 10;
 let b = 5;
 console.log("Sum:", a + b);
 console.log("Difference:", a - b);
+console.log("Product:", a * b);
+console.log("Quotient:", a / b);
 // // let x = 5;
 // // let y = 7;
 // // console.log(`Total is ${x + y}`);
