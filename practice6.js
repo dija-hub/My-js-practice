8f83f6948f2c51719bde4341639874728a747b99
dija <dija5408@gmail.com>
2025-11-17 18:14:18 +0500

    promises

--- practice6.js
+++ practice6.js
@@ -14,45 +14,54 @@
 // const you=new superhuman();
 
 
-const person ={
-      talk(){
-            return"talkink";
-      }
-}
-const me =Object.create(person);
+// const person ={
+//       talk(){
+//             return"talkink";
+//       }
+// }
+// const me =Object.create(person);
 
-let first = "Web";
-let second = "Developer";
-console.log(`${first} ${second}`);
+// let first = "Web";
+// let second = "Developer";
+// console.log(`${first} ${second}`);
 
-let item = "pen";
-let price = 20;
-console.log(`The ${item} costs ${price} rupees.`);
+// let item = "pen";
+// let price = 20;
+// console.log(`The ${item} costs ${price} rupees.`);
 
-let fruits = ["apple", "banana"];
-console.log(`I like ${fruits[1]}`);
+// let fruits = ["apple", "banana"];
+// console.log(`I like ${fruits[1]}`);
 
-let p5 = new Promise((resolve, reject)=>{
-  let ok = false;
-  if(ok) resolve("Success");
-  else reject("Error happened");
-});
+// let p5 = new Promise((resolve, reject)=>{
+//   let ok = false;
+//   if(ok) resolve("Success");
+//   else reject("Error happened");
+// });
 
-p5.then(msg => console.log(msg))
-  .catch(err => console.log(err));
+// p5.then(msg => console.log(msg))
+//   .catch(err => console.log(err));
 
-  function add(a,b){
-  return new Promise(resolve=>{
-    resolve(a + b);
-  });
-}
+//   function add(a,b){
+//   return new Promise(resolve=>{
+//     resolve(a + b);
+//   });
+// }
 
-add(4,5).then(sum => console.log(sum));
+// add(4,5).then(sum => console.log(sum));
+
+// function greet(name){
+//   return new Promise(resolve=>{
+//     resolve(`Welcome ${name}!`);
+//   });
+// }
+
+// greet("Khadija").then(msg => console.log(msg));
 
 function greet(name){
-  return new Promise(resolve=>{
-    resolve(`Welcome ${name}!`);
-  });
+  console.log("hello",name)
+}
+function name(){
+  console.log("khadija")
 }
 
-greet("Khadija").then(msg => console.log(msg));
+greet (name);
