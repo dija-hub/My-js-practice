03cbff24b597982deb8844e829b79b288c398661
dija-hub <dija5408@gmail.com>
2025-11-20 22:05:00 +0500

    practice

--- javascript/practice6.js
+++ javascript/practice6.js
@@ -16,14 +16,15 @@
 // }
 
 
-let fruits = ["apple", "banana"];
-console.log(`My fruit: ${fruits[1]}`);
+// let fruits = ["apple", "banana"];
+// console.log(`My fruit: ${fruits[1]}`);
 
-let age = 18;
-console.log(`You are ${age >= 18 ? "Adult" : "Minor"}`);
+// let age = 18;
+// console.log(`You are ${age >= 18 ? "Adult" : "Minor"}`);
 
-let city = "Karachi";
-console.log(`Welcome to ${city}!`);
+// let city = "Karachi";
+// console.log(`Welcome to ${city}!`);
+
 // class Person{
 //       talk(){
 //             return"talking"
