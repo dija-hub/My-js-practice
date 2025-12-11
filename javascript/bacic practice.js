026671ab36666541fcd1fad42ad3e86b73011b1d
dija-hub <dija5408@gmail.com>
2025-12-11 10:00:00 +0500

    basic practice

--- javascript/bacic practice.js
+++ javascript/bacic practice.js
@@ -1,77 +1,80 @@
-document.body.innerHTML ="good morning";
+// document.body.innerHTML ="good morning";
 
-let tshirt=10;
-let socks=8;
-let dinner_plates=20;
-let total_cost=tshirt+socks+dinner_plates;
-console.log(total_cost);
+// let tshirt=10;
+// let socks=8;
+// let dinner_plates=20;
+// let total_cost=tshirt+socks+dinner_plates;
+// console.log(total_cost);
 
-let student = {
-  name: "Khadija",
-  age: 16
-};
+// let student = {
+//   name: "Khadija",
+//   age: 16
+// };
 
-console.log(student.name); 
+// console.log(student.name); 
 
-function greet() {
-  console.log("Hello!");
-}
-greet();
+// function greet() {
+//   console.log("Hello!");
+// }
+// greet();
 
-let fruits = ["apple", "mango", "grapes"];
-console.log(fruits[0]); 
-
-
-let car = {
-  brand: "Toyota",
-  start: function() {
-    console.log("Car started");
-  }
-};
-
-car.start();
-
-function showFruits(arr) {
-  console.log("Your fruits:", arr);
-}
-
-showFruits(["banana", "orange"]);
-
-let books = [
-  { title: "Math", pages: 100 },
-  { title: "Science", pages: 120 }
-];
-
-console.log(books[1].title); 
-
-function makePerson(name, age) {
-  return {
-    personName: name,
-    personAge: age
-  };
-}
-
-let p1 = makePerson("Khadija", 16);
-console.log(p1);
+// let fruits = ["apple", "mango", "grapes"];
+// console.log(fruits[0]); 
 
 
-function showNames(list) {
-  for (let name of list) {
-    console.log("Name:", name);
-  }
-}
+// let car = {
+//   brand: "Toyota",
+//   start: function() {
+//     console.log("Car started");
+//   }
+// };
 
-showNames(["Ali", "Sara", "Zara"]);
+// car.start();
 
-function showNames(list) {
-  for (let name of list) {
-    console.log("Name:", name);
-  }
-}
+// function showFruits(arr) {
+//   console.log("Your fruits:", arr);
+// }
 
-showNames(["Ali", "Sara", "Zara"]);
+// showFruits(["banana", "orange"]);
+
+// let books = [
+//   { title: "Math", pages: 100 },
+//   { title: "Science", pages: 120 }
+// ];
+
+// console.log(books[1].title); 
+
+// function makePerson(name, age) {
+//   return {
+//     personName: name,
+//     personAge: age
+//   };
+// }
+
+// let p1 = makePerson("Khadija", 16);
+// console.log(p1);
+
+
+// function showNames(list) {
+//   for (let name of list) {
+//     console.log("Name:", name);
+//   }
+// }
+
+// showNames(["Ali", "Sara", "Zara"]);
+
+// function showNames(list) {
+//   for (let name of list) {
+//     console.log("Name:", name);
+//   }
+// }
+
+// showNames(["Ali", "Sara", "Zara"]);
 
 
 
-let car = { brand: "Honda", color: "red" };
-console.log(car.brand);
+// let car = { brand: "Honda", color: "red" };
+// console.log(car.brand);
+
+let fruits = ["apple", "mango", "banana"];
+console.log(fruits[1]);
