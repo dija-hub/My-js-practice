5e8210ba4e45bc443cd4a157c08cec2f7755d1e6
dija-hub <dija5408@gmail.com>
2025-12-12 21:49:22 +0500

    basic pracatice

---
+++ games/script.js
@@ -0,0 +1,26 @@
+let num =Math.floor(Math.random()*7)
+
+let balance=0;
+if(num == 0){
+  console.log("You got zero coins");
+   console.log("Balance:",balance=balance);
+}else if(num==1){
+  console.log("You got 10 coins")
+  console.log("Balance:",balance=balance+10);
+}else if(num==2){
+  console.log("You got 20 coins")
+   console.log("Balance:",balance=balance+20);
+}else if(num==3){
+   console.log("You got 30 coins")
+   console.log("Balance:",balance=balance+30);
+}else if(num==4){
+  console.log("You got 40 coins")
+   console.log("Balance:",balance=balance+40);
+}else if(num==5){
+  console.log("You got 50 coins")
+   console.log("Balance:",balance=balance+50);
+}else if(num==6){
+  console.log("You got 60 coins")
+   console.log("Balance:",balance=balance+60);
+}
+
