d5e9a2cbdfc3c6a6a2a08e52a71cb0e734d3d546
dija-hub <dija5408@gmail.com>
2025-12-12 22:24:31 +0500

    basic pracatice

--- javascript/bacic practice.js
+++ javascript/bacic practice.js
@@ -5,3 +5,10 @@ let socks=8;
 let dinner_plates=20;
 let total_cost=tshirt+socks+dinner_plates;
 console.log(total_cost);
+
+let student = {
+  name: "Khadija",
+  age: 16
+};
+
+console.log(student.name); 
