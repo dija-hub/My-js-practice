4afa7210cd5f3ea69b0523feba944fdba59d25ef
dija-hub <dija5408@gmail.com>
2025-12-21 10:00:00 +0500

    practice

--- javascript/bacic practice.js
+++ javascript/bacic practice.js
@@ -136,11 +136,11 @@
 //     }
 // }
 
-// for (let i=1;i<=10;i++){
-//     if(i>5){
-//         console.log(i)
-//     }
-// }
+for (let i=1;i<=10;i++){
+    if(i>5){
+        console.log(i)
+    }
+}
 
 for(let i=1;i<=10;i++){
     console.log("smartphone",i,"arrived")
