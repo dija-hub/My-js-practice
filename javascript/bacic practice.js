c8de29a2ad3650548744343f794d0d1c9781489e
dija-hub <dija5408@gmail.com>
2025-12-24 18:27:02 +0500

    practice

--- javascript/bacic practice.js
+++ javascript/bacic practice.js
@@ -126,6 +126,12 @@
 // let NF=fruits.filter(fruits.length < 5);
 // console.log(NF)
 
-for(let i=1;i<=5;i++){
-    console.log("student",i)
+// for(let i=1;i<=5;i++){
+//     console.log("student",i)
+// }
+
+for (let i=1;i<=10;i++){
+    if(i%2===0){
+    console.log( i)
+    }
 }
