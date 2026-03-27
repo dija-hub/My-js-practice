b83b6d356a20919e7b183fd2e35f13be4194baaa
Khadija-Dev <dija5408@gmail.com>
2026-03-27 22:03:33 +0500

    loops

--- .js/javascript/practice6.js
+++ .js/javascript/practice6.js
@@ -1676,14 +1676,18 @@
 //     i++
 // }
 
-let j=2;
-while(j <= 10){
-    console.log(j)
-    j+=2
-}
+// let j=2;
+// while(j <= 10){
+//     console.log(j)
+//     j+=2
+// }
 
-let i=1
-do{
-    console.log(i)
-    i++
-}while(i<=5)
+// let i=1
+// do{
+//     console.log(i)
+//     i++
+// }while(i<=5)
+
+for (let i=10;i>=1;i--){
+console.log(i)
+}
